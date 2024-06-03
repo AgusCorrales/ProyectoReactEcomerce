27eb91d00618e45689bfde45ce5a400530f6c86e
AgusCorrales <aguscorrales1997@gmail.com>
2024-06-03 02:06:24 +0200

    Ultimos detalles

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,147 +4,147 @@ import { Link } from 'react-router-dom'
 import { Carousel } from 'antd';
 
 const Home = () => {
-      const contentStyle = {
+    const contentStyle = {
         marginTop: '10px',
         height: '160px',
         lineHeight: '160px',
-      };
-  return (
-    <div>
-      <div>
-      <Carousel autoplay>
-      <div>
-        <img src='https://images.pexels.com/photos/5632378/pexels-photo-5632378.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' 
-        style={{ ...contentStyle, objectFit: 'cover', width: '100%', height: '500px', marginTop:'50px'}}></img>
-      </div>
-      <div>
-      <img src='https://images.pexels.com/photos/139038/pexels-photo-139038.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' 
-      style={{ ...contentStyle, objectFit: 'cover', width: '100%', height: '500px', marginTop:'50px' }}></img>
-      </div>
-      <div>
-      <img src='https://images.pexels.com/photos/3945683/pexels-photo-3945683.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' 
-      style={{ ...contentStyle, objectFit: 'cover', width: '100%', height: '500px', marginTop:'50px' }}></img>
-      </div>
-      <div>
-      <img src='https://images.pexels.com/photos/5624976/pexels-photo-5624976.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' 
-      style={{ ...contentStyle, objectFit: 'cover', width: '100%', height: '500px', marginTop:'50px' }}></img>
-      </div>
-    </Carousel>
-       </div>
-       
-        <div className="giphy-container">
-            <div className='xbox'>
-          <iframe 
-            src="https://giphy.com/embed/gHWT7cA7rRxwagVsPa" 
-            width="890" 
-            height="500" 
-            frameBorder="0" 
-            className="giphy-embed" 
-            allowFullScreen>
-          </iframe>
-          <Link to="product">
-            <div className="button" >
-              <div className="button-wrapper">
-                <span className="icon">
-                  <svg viewBox="0 0 16 16" className="bi bi-cart2" fill="currentColor" height="16" width="16" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l1.25 5h8.22l1.25-5H3.14zM5 13a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z"></path>
-                  </svg>
-                </span>
-              </div>
+    };
+    return (
+        <div>
+            <div>
+                <Carousel autoplay>
+                    <div>
+                        <img src='https://images.pexels.com/photos/5632378/pexels-photo-5632378.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
+                            style={{ ...contentStyle, objectFit: 'cover', width: '100%', height: '500px', marginTop: '50px' }}></img>
+                    </div>
+                    <div>
+                        <img src='https://images.pexels.com/photos/139038/pexels-photo-139038.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
+                            style={{ ...contentStyle, objectFit: 'cover', width: '100%', height: '500px', marginTop: '50px' }}></img>
+                    </div>
+                    <div>
+                        <img src='https://images.pexels.com/photos/3945683/pexels-photo-3945683.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
+                            style={{ ...contentStyle, objectFit: 'cover', width: '100%', height: '500px', marginTop: '50px' }}></img>
+                    </div>
+                    <div>
+                        <img src='https://images.pexels.com/photos/5624976/pexels-photo-5624976.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
+                            style={{ ...contentStyle, objectFit: 'cover', width: '100%', height: '500px', marginTop: '50px' }}></img>
+                    </div>
+                </Carousel>
             </div>
-          </Link>
-          </div>
+
+            <div className="giphy-container">
+                <div className='xbox'>
+                    <iframe
+                        src="https://giphy.com/embed/gHWT7cA7rRxwagVsPa"
+                        width="890"
+                        height="500"
+                        frameBorder="0"
+                        className="giphy-embed"
+                        allowFullScreen>
+                    </iframe>
+                    <Link to="product">
+                        <div className="button" >
+                            <div className="button-wrapper">
+                                <span className="icon">
+                                    <svg viewBox="0 0 16 16" className="bi bi-cart2" fill="currentColor" height="16" width="16" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l1.25 5h8.22l1.25-5H3.14zM5 13a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z"></path>
+                                    </svg>
+                                </span>
+                            </div>
+                        </div>
+                    </Link>
+                </div>
+            </div>
+
+            <div className="giphy-container">
+                <iframe
+                    src="https://giphy.com/embed/Sl1PEQk8fKeqcPDCEl"
+                    width="700"
+                    height="400"
+                    frameBorder="0"
+                    className="giphy-embed"
+                    allowFullScreen>
+                </iframe>
+                <Link to="product">
+                    <div data-tooltip="Price: $500" className="button">
+                        <div className="button-wrapper">
+                            <span className="icon">
+                                <svg viewBox="0 0 16 16" className="bi bi-cart2" fill="currentColor" height="16" width="16" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l1.25 5h8.22l1.25-5H3.14zM5 13a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z"></path>
+                                </svg>
+                            </span>
+                        </div>
+                    </div>
+                </Link>
+            </div>
+
+            <div className="giphy-container">
+                <iframe
+                    src="https://giphy.com/embed/8sJgpqqYTlOPm"
+                    width="700"
+                    height="400"
+                    frameBorder="0"
+                    className="giphy-embed"
+                    allowFullScreen>
+                </iframe>
+                <Link to="product">
+                    <div className="button">
+                        <div className="button-wrapper">
+                            <span className="icon">
+                                <svg viewBox="0 0 16 16" className="bi bi-cart2" fill="currentColor" height="16" width="16" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l1.25 5h8.22l1.25-5H3.14zM5 13a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z"></path>
+                                </svg>
+                            </span>
+                        </div>
+                    </div>
+                </Link>
+            </div>
+            <div className="giphy-container">
+                <iframe
+                    src="https://giphy.com/embed/irSg1eFKZVIfkh958u"
+                    width="500"
+                    height="500"
+                    frameBorder="0"
+                    className="giphy-embed"
+                    allowFullScreen>
+                </iframe>
+                <Link to="product">
+                    <div className="button">
+                        <div className="button-wrapper">
+                            <span className="icon">
+                                <svg viewBox="0 0 16 16" className="bi bi-cart2" fill="currentColor" height="16" width="16" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l1.25 5h8.22l1.25-5H3.14zM5 13a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z"></path>
+                                </svg>
+                            </span>
+                        </div>
+                    </div>
+                </Link>
+            </div>
+            <div className="giphy-container">
+                <iframe
+                    src="https://gifer.com/embed/2bsy"
+                    width="900"
+                    height="500"
+                    frameBorder="0"
+                    className="giphy-embed"
+                    allowFullScreen>
+                </iframe>
+                <Link to="product">
+                    <div className="button">
+                        <div className="button-wrapper">
+                            <span className="icon">
+                                <svg viewBox="0 0 16 16" className="bi bi-cart2" fill="currentColor" height="16" width="16" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l1.25 5h8.22l1.25-5H3.14zM5 13a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z"></path>
+                                </svg>
+                            </span>
+                        </div>
+                    </div>
+                </Link>
+            </div>
+
+
         </div>
-
-        <div className="giphy-container">
-          <iframe 
-            src="https://giphy.com/embed/Sl1PEQk8fKeqcPDCEl" 
-            width="700" 
-            height="400" 
-            frameBorder="0" 
-            className="giphy-embed" 
-            allowFullScreen>
-          </iframe>
-          <Link to="product">
-            <div data-tooltip="Price: $500" className="button">
-              <div className="button-wrapper">
-                <span className="icon">
-                  <svg viewBox="0 0 16 16" className="bi bi-cart2" fill="currentColor" height="16" width="16" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l1.25 5h8.22l1.25-5H3.14zM5 13a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z"></path>
-                  </svg>
-                </span>
-              </div>
-            </div>
-          </Link>
-        </div>
-
-        <div className="giphy-container">
-        <iframe 
-          src="https://giphy.com/embed/8sJgpqqYTlOPm" 
-          width="700" 
-          height="400" 
-          frameBorder="0" 
-          className="giphy-embed" 
-          allowFullScreen>
-        </iframe>
-        <Link to="product">
-          <div className="button">
-            <div className="button-wrapper">
-              <span className="icon">
-                <svg viewBox="0 0 16 16" className="bi bi-cart2" fill="currentColor" height="16" width="16" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l1.25 5h8.22l1.25-5H3.14zM5 13a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z"></path>
-                </svg>
-              </span>
-            </div>
-          </div>
-        </Link>
-      </div>
-      <div className="giphy-container">
-        <iframe 
-          src="https://giphy.com/embed/irSg1eFKZVIfkh958u"  
-          width="500" 
-          height="500" 
-          frameBorder="0" 
-          className="giphy-embed" 
-          allowFullScreen>
-        </iframe>
-        <Link to="product">
-          <div className="button">
-            <div className="button-wrapper">
-              <span className="icon">
-                <svg viewBox="0 0 16 16" className="bi bi-cart2" fill="currentColor" height="16" width="16" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l1.25 5h8.22l1.25-5H3.14zM5 13a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z"></path>
-                </svg>
-              </span>
-            </div>
-          </div>
-        </Link>
-      </div>
-      <div className="giphy-container">
-        <iframe 
-          src="https://gifer.com/embed/2bsy"  
-          width="900" 
-          height="500" 
-          frameBorder="0" 
-          className="giphy-embed" 
-          allowFullScreen>
-        </iframe>
-        <Link to="product">
-          <div className="button">
-            <div className="button-wrapper">
-              <span className="icon">
-                <svg viewBox="0 0 16 16" className="bi bi-cart2" fill="currentColor" height="16" width="16" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l1.25 5h8.22l1.25-5H3.14zM5 13a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z"></path>
-                </svg>
-              </span>
-            </div>
-          </div>
-        </Link>
-      </div>
-      
-
-    </div>
-  );
+    );
 }
 
 export default Home
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -7,91 +7,91 @@ import './Login.scss'
 
 
 const Login = () => {
-  const {login, user} = useContext(UserContext)
-  const emailRegex = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/
+    const { login, user } = useContext(UserContext)
+    const emailRegex = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/
 
-  const navigate = useNavigate()
-  const onFinish = async (values) => {
-    await login(values)
-    console.log(user);
-    navigate("/profile")
-     notification.success({
-       message: 'Welcome'
-    });
-  }
+    const navigate = useNavigate()
+    const onFinish = async (values) => {
+        await login(values)
+        console.log(user);
+        navigate("/profile")
+        notification.success({
+            message: 'Welcome'
+        });
+    }
 
-  const onFinishFailed = (values) => {
-    console.log(values);
-  };
-     
+    const onFinishFailed = (values) => {
+        console.log(values);
+    };
 
-  return (
-    <div className='form-container'>
-      <Form
-        name="basic"
-        labelCol={{
-          span: 8,
-        }}
-        wrapperCol={{
-          span: 16,
-        }}
-        style={{
-          maxWidth: 600,
-        }}
-        initialValues={{
-          remember: true,
-        }}
-        onFinish={onFinish}
-        onFinishFailed={onFinishFailed}
-        autoComplete="on"
-      >
-        <Form.Item
-          label="Email"
-          name="email"
-          rules={[
-            {
-              required: true,
-              message: 'Please input your email!',
-            },
-            {
-              pattern: emailRegex,
-              message: 'Please input a correct email!',
-            },
-          ]}
-        >
-          <Input />
-        </Form.Item>
 
-        <Form.Item
-          label="Password"
-          name="password"
-          rules={[
-            {
-              required: true,
-              message: 'Please input your password!',
-            },
-          ]}
-        >
-          <Input.Password />
-        </Form.Item>
+    return (
+        <div className='form-container'>
+            <Form
+                name="basic"
+                labelCol={{
+                    span: 8,
+                }}
+                wrapperCol={{
+                    span: 16,
+                }}
+                style={{
+                    maxWidth: 600,
+                }}
+                initialValues={{
+                    remember: true,
+                }}
+                onFinish={onFinish}
+                onFinishFailed={onFinishFailed}
+                autoComplete="on"
+            >
+                <Form.Item
+                    label="Email"
+                    name="email"
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Please input your email!',
+                        },
+                        {
+                            pattern: emailRegex,
+                            message: 'Please input a correct email!',
+                        },
+                    ]}
+                >
+                    <Input />
+                </Form.Item>
 
-        <Form.Item
-          wrapperCol={{
-            offset: 8,
-            span: 16,
-          }}
-        >
-        <div className='button-container'>
-          <Button type="primary" htmlType="submit">
-            Submit
-          </Button>
-          Or <a href="/register"> Register now!</a>
-        </div>  
-        </Form.Item>
+                <Form.Item
+                    label="Password"
+                    name="password"
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Please input your password!',
+                        },
+                    ]}
+                >
+                    <Input.Password />
+                </Form.Item>
 
-      </Form>
-    </div>
-  );
+                <Form.Item
+                    wrapperCol={{
+                        offset: 8,
+                        span: 16,
+                    }}
+                >
+                    <div className='button-container'>
+                        <Button type="primary" htmlType="submit">
+                            Submit
+                        </Button>
+                        Or <a href="/register"> Register now!</a>
+                    </div>
+                </Form.Item>
+
+            </Form>
+        </div>
+    );
 };
 
 export default Login;
